70ed6ceebb84b7c8b5b8ef9be471a926162b569a
freeslugs <freeslugs@gmail.com>
2014-10-18 21:41:24 -0400

    implement flask-restful

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 import requests
 from flask import Flask
+from flask.ext import restful
 
 app = Flask(__name__)
+api = restful.Api(app)
 
-@app.route('/')
-def hello_world():
-	token = get_access_token()
-	tags = get_tags(token)
-	print tags
-	return "done! "
+
+class HelloWorld(restful.Resource):
+  def get(self):
+    token = get_access_token()
+    tags = get_tags(token)
+    return tags
+
+api.add_resource(HelloWorld, '/')
+
 
 def get_access_token():
 	r = requests.post("https://api.clarifai.com/v1/token/?grant_type=client_credentials&client_id=urXuu-oV3NBj4WL8WUW37cPWlc1qDImDndJeDCc-&client_secret=LVqjz3Jhjb3xrM1IktglejjXLHZ-9e5QLJknvZAQ")
@@ -19,6 +24,5 @@ def get_tags(token, url="http://www.clarifai.com/img/metro-north.jpg"):
 	r = requests.get('https://api.clarifai.com/v1/tag/?url='+url+'&access_token='+token)
 	return r.json()
 
-	
 if __name__ == '__main__':
 	app.run()
