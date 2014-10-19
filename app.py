58f36c5144a73f78a4faee56f5636bcf44166347
freeslugs <freeslugs@gmail.com>
2014-10-18 22:09:10 -0400

    refactor code

--- app.py
+++ app.py
@@ -5,22 +5,42 @@ from flask.ext import restful
 app = Flask(__name__)
 api = restful.Api(app)
 
+### USER GUI ###
 
-class HelloWorld(restful.Resource):
+@app.route('/')
+def hello_world():
+  return 'Hello World!'
+
+
+### API ###
+
+class Clarifai(restful.Resource):
   def get(self):
-    token = get_access_token()
-    tags = get_tags(token)
+    token = clarifai_get_access_token()
+    tags = clarifai_get_tags(token)
     return tags
 
-api.add_resource(HelloWorld, '/')
+
+api.add_resource(Clarifai, '/clarifai')
 
 
-def get_access_token():
+
+
+# function for getting a code from user
+# essentially user logins 
+# we need to handle the callback
+# extract the code
+# use the code in another request ot get the acces token
+# save that for a user
+# use the token to make requests for pics
+# 
+
+def clarifai_get_access_token():
 	r = requests.post("https://api.clarifai.com/v1/token/?grant_type=client_credentials&client_id=urXuu-oV3NBj4WL8WUW37cPWlc1qDImDndJeDCc-&client_secret=LVqjz3Jhjb3xrM1IktglejjXLHZ-9e5QLJknvZAQ")
 	access_token = r.json()['access_token']
 	return access_token
 
-def get_tags(token, url="http://www.clarifai.com/img/metro-north.jpg"):
+def clarifai_get_tags(token, url="http://www.clarifai.com/img/metro-north.jpg"):
 	r = requests.get('https://api.clarifai.com/v1/tag/?url='+url+'&access_token='+token)
 	return r.json()
 
