3a6df59bfe9793aacd0a363de5f8f8d7a033c3d0
System Administrator <root@sherlock.is.private>
2014-10-19 00:02:08 -0400

    get_images_instagram function written and working

--- app.py
+++ app.py
@@ -16,7 +16,10 @@ def hello_world():
 def instagram_callback():
   code = request.args['code']
   token = get_access_token_instagram(code)
+  # pics = get_images_instagram(token)
   return token
+  #images = get_images_instagram(token)
+  #return images
   # save_token 
   # render gallery
   
@@ -29,8 +32,19 @@ def instagram_callback():
 def get_access_token_instagram(code):
 	payload = {'client_id': '96eea83eeeed431490a2997dcb597d22', 'client_secret': 'd9a08fd300854c9e9da1a4fe2035db1b', 'grant_type': 'authorization_code', 'redirect_uri': 'http://127.0.0.1:5000/instagram-redirect', 'code':code}
 	r = requests.post("https://api.instagram.com/oauth/access_token", data=payload)
+	print r.json()
 	access_token = r.json()['access_token']
 	return access_token
+
+def get_images_instagram(access_token):
+	r = requests.get("https://api.instagram.com/v1/users/self/media/recent?access_token="+access_token)
+	images = r.json()
+	array = []
+	for image in images['data']:
+		array.append(image['images']['standard_resolution']['url'])
+	return array
+	# return images['data'][0]['images']['standard_resolution']['url']
+	# return images
 
 
 ### API ###
@@ -41,7 +55,11 @@ class Clarifai(restful.Resource):
     tags = clarifai_get_tags(token)
     return tags
 
+class Instagram(restful.Resource):
+  def get(self):
+    return get_images_instagram("403542871.96eea83.6e3ef7b63c1e4ebf86ff2d171f856a33")
 
+api.add_resource(Instagram, '/instagram')
 api.add_resource(Clarifai, '/clarifai')
 
 
